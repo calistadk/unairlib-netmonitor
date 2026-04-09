111771a0369110d59907a81bb207b0f77edb6794
calistadk <calista.dian.kemala-2023@fst.unair.ac.id>
2026-04-09 15:49:23 +0700

    revisi monitoring detail

--- resources/views/monitoringdetail.blade.php
+++ resources/views/monitoringdetail.blade.php
@@ -173,19 +173,6 @@
                     <span class="font-semibold text-sm {{ $colors['head'] }}">
                         Tentang Grafik: {{ $info['title'] }}
                     </span>
-
-                    {{-- AI Analyze Button --}}
-                    <button
-                        onclick="analyzeGraph('{{ $gid }}', '{{ addslashes($graph['name']) }}', '{{ $host['nama'] }}')"
-                        id="btn-analyze-{{ $gid }}"
-                        class="flex items-center gap-1.5 px-3 py-1.5 text-xs font-semibold text-white
-                               {{ $colors['btn'] }} rounded-lg transition">
-                        <svg class="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"/>
-                        </svg>
-                        Analyze with AI
-                    </button>
                 </div>
 
                 {{-- Static explanation --}}
