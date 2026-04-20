5c2aef5f26472ffdd973118a776dde84f7685df5
calistadk <calista.dian.kemala-2023@fst.unair.ac.id>
2026-04-20 11:16:49 +0700

    edit password di login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -50,13 +50,33 @@
 
             <div>
                 <label class="text-gray-700 text-sm">Password</label>
-                <input type="password" name="password"
-                    placeholder="Masukkan password"
-                    class="w-full mt-1 px-4 py-3 rounded-xl border bg-gray-200 focus:outline-none">
-            </div>
+                <div class="relative">
+                    <input type="password" name="password" id="password"
+                        placeholder="Masukkan password"
+                        class="w-full mt-1 px-4 py-3 pr-12 rounded-xl border bg-gray-200 focus:outline-none">
 
-            <div class="text-right">
-                <a href="#" class="text-sm text-blue-600">Lupa Password?</a>
+                    <button type="button" id="togglePassword"
+                        class="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 focus:outline-none">
+
+                        {{-- Ikon mata (password tersembunyi) --}}
+                        <svg id="eyeIcon" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/>
+                            <circle cx="12" cy="12" r="3"/>
+                        </svg>
+
+                        {{-- Ikon mata dicoret (password terlihat) --}}
+                        <svg id="eyeOffIcon" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 hidden" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.94 17.94A10.07 10.07 0 0112 20c-7 0-11-8-11-8a18.45 18.45 0 015.06-5.94"/>
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9.9 4.24A9.12 9.12 0 0112 4c7 0 11 8 11 8a18.5 18.5 0 01-2.16 3.19"/>
+                            <line x1="1" y1="1" x2="23" y2="23" stroke-linecap="round"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
 
             <button type="submit"
@@ -71,6 +91,20 @@
         </p>
 
     </div>
+
+    <script>
+        const togglePassword = document.getElementById('togglePassword');
+        const passwordInput  = document.getElementById('password');
+        const eyeIcon        = document.getElementById('eyeIcon');
+        const eyeOffIcon     = document.getElementById('eyeOffIcon');
+
+        togglePassword.addEventListener('click', () => {
+            const isHidden = passwordInput.type === 'password';
+            passwordInput.type = isHidden ? 'text' : 'password';
+            eyeIcon.classList.toggle('hidden', isHidden);
+            eyeOffIcon.classList.toggle('hidden', !isHidden);
+        });
+    </script>
 
 </body>
 </html>
